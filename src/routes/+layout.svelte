86c4ad50e3d3c1168697d8141db71010a8de6504
Tlesis <jackwilliamson.1215@gmail.com>
2023-08-31 22:50:10 -0500

    robotics site rewrite

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,41 +1,60 @@
 <script lang="ts">
     import "../app.css";
+    export const prerender = true;
+
+    const links = [
+        /* {
+            name: "Home",
+            href: "/",
+            target: "_self",
+        }, */
+        {
+            name: "Calendar",
+            href: "https://www.usd232.org/Page/5145",
+            target: "_blank",
+        },
+        {
+            name: "Photo Gallery",
+            href: "/photos",
+            target: "_self",
+        },
+        {
+            name: "Sponsors",
+            href: "/sponsors",
+            target: "_self",
+        },
+        {
+            name: "Regional Info",
+            href: "/regional-info",
+            target: "_self",
+        },
+        {
+            name: "Links & Articles",
+            href: "/link-articles",
+            target: "_self",
+        },
+        {
+            name: "Forms",
+            href: "/forms",
+            target: "_self",
+        },
+    ];
+
+
 </script>
 
 <nav class="flex justify-evenly items-center bg-fg text-w font-bold h-[8vh]">
-    <h1 class="uppercase letterspace-wide text-xl group">
+    <a href="/" class="uppercase letterspace-wide text-xl group">
         Catatronics
-        <span class="block max-w-0 group-hover:max-w-full transition-all duration-300 h-0.5 bg-w max-md:hidden"></span>
-    </h1>
-    <ul class="letterspace-thin relative text-sm flex justify-around w-4/5">
-        <li class="group">
-            <a href="/">Home</a>
-            <span class="block max-w-0 group-hover:max-w-full transition-all duration-300 h-0.5 bg-w max-md:hidden"></span>
-        </li>
-        <li class="group">
-            <a href="https://www.usd232.org/Page/5145" target="_blank">Calendar</a>
-            <span class="block max-w-0 group-hover:max-w-full transition-all duration-300 h-0.5 bg-w max-md:hidden"></span>
-        </li>
-        <li class="group">
-            <a href="https://www.usd232.org/Page/6757" target="_blank">Regional Info</a>
-            <span class="block max-w-0 group-hover:max-w-full transition-all duration-300 h-0.5 bg-w max-md:hidden"></span>
-        </li>
-        <li class="group">
-            <a href="/photos">Photo Gallery</a>
-            <span class="block max-w-0 group-hover:max-w-full transition-all duration-300 h-0.5 bg-w max-md:hidden"></span>
-        </li>
-        <li class="group">
-            <a href="https://www.usd232.org/Page/8007" target="_blank">Links & Articles</a>
-            <span class="block max-w-0 group-hover:max-w-full transition-all duration-300 h-0.5 bg-w max-md:hidden"></span>
-        </li>
-        <li class="group">
-            <a href="https://www.usd232.org/Page/6811" target="_blank">Forms</a>
-            <span class="block max-w-0 group-hover:max-w-full transition-all duration-300 h-0.5 bg-w max-md:hidden"></span>
-        </li>
-        <li class="group">
-            <a href="/sponsors">Sponsors</a>
-            <span class="block max-w-0 group-hover:max-w-full transition-all duration-300 h-0.5 bg-w max-md:hidden"></span>
-        </li>
+        <span class="block max-w-0 group-hover:max-w-full transition-all duration-300 h-0.5 bg-w max-lg:hidden"></span>
+    </a>
+    <ul class="nav-links letterspace-thin font-thin relative text-sm hidden justify-around w-4/5 lg:flex">
+        {#each links as link}
+            <li class="group">
+                <a href={link.href} target={link.target}>{link.name}</a>
+                <span class="block max-w-0 group-hover:max-w-full transition-all duration-300 h-px bg-w max-lg:hidden"></span>
+            </li>
+        {/each}
     </ul>
 </nav>
 
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,38 +3,67 @@
 </svelte:head>
 
 <div class="overflow-hidden h-[85vh]">
-    <div class="bg-home bg-fixed bg-center bg-no-repeat bg-cover h-full w-full blur-md"></div>
+    <div class="bg-home bg-fixed bg-center bg-no-repeat bg-cover h-full w-full blur"></div>
 </div>
 
-<div class="container text-w font-bold backdrop-blur-lg border-2 border-w bg-[#00000099] p-5">
-    <h1 class="text-center text-[2.5rem]">hello, this is [Team 1810]</h1>
-    <h2 class="text-center text-base border-b border-w pb-4">(this page is under construction)</h2>
+<div class="container text-w font-bold backdrop-blur-3xl border-2 border-w bg-[#00000099] p-5">
+    <h1 class="text-center text-[2.5rem]">Hello, This is FRC Team<br>1810 & 9316</h1>
+    <hr class="mt-4">
+    <!-- <h2 class="text-center text-base border-b border-w pb-4"></h2> -->
 </div>
 
-<div class="bg-fg px-12 py-12">
-    <h3 class="text-center text-3xl font-bold">for now check out:</h3>
+<div class="text-w bg-fg px-12 pb-12">
+    <h1 class="text-center text-3xl font-bold p-8">About Us:</h1>
+    <div class="flex flex-wrap justify-center gap-3 mb-8">
+        <p class="w-5/6">
+            &emsp;FIRST (For Inspiration and Recognition of Science and Technology) Robotics Teams 1810 & 9316, aka 1810 Catatronics and 9316 Cubatronics, is comprised of students from both the DeSoto and Mill Valley high schools in USD232.
+            The students work together to build a robot that solves the challenges presented by FIRST Robotics in each year's game, thereby expanding our knowledge of STEM principles.
+            Our team is comprised of students that focus on CAD & design, manufacturing, electrical, programming, scouting & strategy, and business sub-teams.
+            Students are encouraged to try all the sub-teams.
+            We strive to work cohesively as a team, ensuring that each student is able to take these experiences into their future career paths.
+        </p>
+        <p class="w-5/6">
+            Teams design, build, and program industrial-sized robots to play a difficult field game against like-minded competitors.
+            We have six weeks to accomplish that goal.
+            Our focus is to promote STEM in our school and community and is achieved through outreach projects, fundraising, and presentations.
+        </p>
+    </div>
+    <hr class="mt-4">
+
+    <!-- <h3 class="text-center text-3xl font-bold">Check Out:</h3>
     <ul class="list-disc list-outside text-xl leading-10">
-        <li>our <a class="italic text-link hover:underline visited:text-link-visited" href="https://www.usd232.org/Domain/3832" target=_blank> district page</a></li>
-        <li>or our <a class="italic text-link hover:underline visited:text-link-visited" href="https://github.com/Team1810Robotics" target=_blank>github</a></li>
+        <li>The <a class="italic text-link hover:underline visited:text-link-visited" href="https://www.usd232.org/Domain/3832" target=_blank> District Page</a> For The Teams</li>
+        <li>The <a class="italic text-link hover:underline visited:text-link-visited" href="https://github.com/Team1810Robotics" target=_blank>Github</a> That Holds Our Code</li>
         <hr class="mt-4">
-    </ul>
-    <h3 class="text-center text-2xl font-bold">contact us:</h3>
-    <ul class="list-disc text-w text-xl leading-10">
-        <li>Neil McLeod</li>
-        <ul class="list-disc ml-12">
-            <li>nmcleod@usd232.org</li>
-            <li>(913) 422-4351 ext 3140</li>
-        </ul>
-        <br>
-        <li>Dianna Worm Mayfield </li>
-        <ul class="list-disc ml-12">
-            <li>dworm@usd232.org</li>
-            <li>(913) 620-2920</li>
-        </ul>
-    </ul>
+    </ul> -->
+
+
+
+    <h3 class="text-center text-2xl font-bold">Contact Us:</h3>
+    <div class="text-xl leading-10 flex justify-evenly">
+        <div>
+            Neil McLeod
+            <ul class="list-disc ml-12">
+                <li>nmcleod@usd232.org</li>
+                <li>(913) 422-4351 ext 3140</li>
+            </ul>
+        </div>
+        <div>
+            Dianna Worm Mayfield
+            <ul class="list-disc ml-12">
+                <li>dworm@usd232.org</li>
+                <li>(913) 620-2920</li>
+            </ul>
+        </div>
+    </div>
 </div>
 
 <style lang="postcss">
+
+    :global(body) {
+        background-color: #0d1117;
+    }
+
     .container {
         /* TODO: use better way to center */
         position: absolute;
